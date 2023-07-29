2ebd3b897cfa646db285ffc9bd47a5aa0c839b62
rishabh <rishabh967537@gmail.com>
2023-07-29 14:49:43 +0530

    commit FindTargetEqualPairSum class.

--- src/FindTargetEqualPairSum.java
+++ src/FindTargetEqualPairSum.java
@@ -1,2 +1,20 @@
+import java.util.HashSet;
+
 public class FindTargetEqualPairSum {
+
+    public static void main(String args[]){
+        int arr[] = {3,4,6,9,10};
+        int target = 10;
+        HashSet<Integer> hset=new HashSet<>();
+        for(int i = 0;i<arr.length;i++){
+            hset.add(arr[i]);
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(hset.contains(target-arr[i])){
+                System.out.println(arr[i]+" "+(target-arr[i]));
+            }
+        }
+    }
+
 }
